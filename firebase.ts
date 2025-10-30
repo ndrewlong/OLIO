f4b714e663e9756f13d8609f874d489b49a29ed8
Andrea <105546251+ndrewlong@users.noreply.github.com>
2025-10-30 20:18:25 +0100

    Create firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCwqRkL8WFxn_Aubry-z2E2jYHU-exh4U",
+  authDomain: "olio-4ac6d.firebaseapp.com",
+  projectId: "olio-4ac6d",
+  storageBucket: "olio-4ac6d.firebasestorage.app",
+  messagingSenderId: "182571556319",
+  appId: "1:182571556319:web:c371acc5a5ad33798f386a"
+};
+
+// Inizializza Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inizializza Firestore
+export const db = getFirestore(app);
